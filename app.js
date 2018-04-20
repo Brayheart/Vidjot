0f78c08bac407f0ede1ee6611e05c44e675e852f
Tyler Bray <broncobray77@gmail.com>
2018-04-20 12:11:48 -0700

    added handlebars

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+
+//handlebars middlewear
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars'); 
+
+//index route
+app.get('/', (req, res) => {
+    const title = 'Welcome11';
+    res.render('index', {
+        title: title
+    });
+})
+
+//about route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+app.listen(3000, () => {
+    console.log('port is listening on port 3000!')
+})
