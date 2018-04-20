4d083b6c75f1142844b5dd2549adaee002fcb178
Tyler Bray <broncobray77@gmail.com>
2018-04-20 14:47:29 -0700

    added Schema

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+
+
 const app = express();
+
+//connect to mongoose
+//if deprication warning set mongoose promise to global promise
+mongoose.connect('mongodb://localhost/vidjot-dev')
+.then(() => console.log('MongoDB connected!'))
+.catch(err => console.log(err));
+
+//Load Idea Model
+require('./models/Ideas');
+const idea = mongoose.model('ideas');
 
 //handlebars middlewear
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
