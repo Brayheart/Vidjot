ef99dd50bbff10b67223ea76852b9b7fdcf8fe35
Tyler Bray <broncobray77@gmail.com>
2018-04-21 14:26:43 -0700

    post to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+
+
 const express = require('express');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 const app = express();
@@ -19,6 +22,15 @@ const idea = mongoose.model('ideas');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars'); 
 
+
+//bodyparser middlewear
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
 //index route
 app.get('/', (req, res) => {
     const title = 'Welcome';
@@ -36,6 +48,36 @@ app.get('/about', (req, res) => {
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
 });
+
+//Process form
+app.post('/ideas', (req, res) => {
+    let errors = [];
+    
+    if (!req.body.title) {
+        errors.push({text: 'Please add a title'})
+    }
+    if (!req.body.details) {
+        errors.push({text: 'Please add some details'})
+    }
+
+    if (errors.length > 0) {
+        res.render('ideas/add', {
+            errors: errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    } else {
+        const newUser = {
+            title: req.body.title,
+            details: req.body.details
+        }
+        new idea(newUser)
+            .save()
+            .then(idea => {
+                res.redirect('/ideas');
+            })
+    }
+})
 
 app.listen(3000, () => {
     console.log('port is listening on port 3000!')
