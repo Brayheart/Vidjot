48ad1f5057cee4773cf75924fdd284af1948ea78
Tyler Bray <broncobray77@gmail.com>
2018-04-21 14:34:30 -0700

    rendered db info

--- app.js
+++ app.js
@@ -44,6 +44,17 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+//Idea Index Page
+app.get('/ideas', (req, res) => {
+    idea.find({})
+        .sort({date:'desc'})
+        .then(ideas => {
+            res.render('ideas/index', {
+                ideas:ideas
+            });
+        })
+})
+
 //Add Idea Form
 app.get('/ideas/add', (req, res) => {
     res.render('ideas/add');
